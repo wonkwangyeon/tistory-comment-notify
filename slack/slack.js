4a161e29dfaa859f310cf107e123df7fe695b496
wonkwangyeon <cocoll2000@naver.com>
2023-02-10 00:03:42 +0900

    fix : modify channel

--- slack/slack.js
+++ slack/slack.js
@@ -4,13 +4,13 @@ const Slack = require('slack-node');
 module.exports = {
 
     sendSlack: function (text) {
-        
+
         const slack = new Slack();
         slack.setWebhook(process.env.WEBHOOK_URI);
-        
+
         slack.webhook({
-            channel: process.env.CHANNEL,
-            username: '#'+process.env.USER_NAME,
+            channel: '#' + process.env.CHANNEL,
+            username: process.env.USER_NAME,
             text: JSON.stringify(text)
         }, function (err, response) {
 
